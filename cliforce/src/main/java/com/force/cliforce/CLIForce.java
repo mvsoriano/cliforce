20e2aa659ed523c13fc1432a6883d5ff566825eb
sclasen <sclasen@salesforce.com>
2011-03-02 11:32:19 -0800

    move wait to right place

--- cliforce/src/main/java/com/force/cliforce/CLIForce.java
+++ cliforce/src/main/java/com/force/cliforce/CLIForce.java
@@ -440,11 +440,11 @@ public class CLIForce {
 
     private void executeWithArgs(String[] cmds) throws InterruptedException {
         String cmdKey = cmds[0];
+        initLatch.await();
         Command cmd = commands.get(cmdKey);
         String[] args = cmds.length > 1 ? Arrays.copyOfRange(cmds, 1, cmds.length) : new String[0];
         if (!cmdKey.equals("") && !cmdKey.equals(EXITCMD)) {
             if (cmd != null) {
-                initLatch.await();
                 executeCommand(cmdKey, cmd, args);
             } else {
                 writer.printf("Unknown Command %s\n", cmdKey);
