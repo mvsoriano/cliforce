0d170377cb0626dca6329250b86390df6b2e7c79
sclasen <sclasen@salesforce.com>
2011-03-15 10:50:37 -0700

    format imports

--- cliforce/src/main/java/com/force/cliforce/JCommand.java
+++ cliforce/src/main/java/com/force/cliforce/JCommand.java
@@ -6,8 +6,6 @@ import com.beust.jcommander.ParameterDescription;
 import com.beust.jcommander.ParameterException;
 import jline.FileNameCompletor;
 import jline.SimpleCompletor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.lang.reflect.ParameterizedType;
@@ -386,7 +384,6 @@ public abstract class JCommand<T> implements Command {
         return true;
     }
 
-  
 
 }
 
