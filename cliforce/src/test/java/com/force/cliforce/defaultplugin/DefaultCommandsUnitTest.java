5bcdc795b50584bf9c90db3c98a6bdfed75394bf
sclasen <sclasen@salesforce.com>
2011-03-28 13:07:21 -0700

    W-922848 test

--- cliforce/src/test/java/com/force/cliforce/defaultplugin/DefaultCommandsUnitTest.java
+++ cliforce/src/test/java/com/force/cliforce/defaultplugin/DefaultCommandsUnitTest.java
@@ -1,11 +1,10 @@
 package com.force.cliforce.defaultplugin;
 
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 import com.force.cliforce.DefaultPlugin;
 import com.force.cliforce.TestCommandContext;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Tests commands in the default plugin that dont require any guice injection.
@@ -20,7 +19,19 @@ public class DefaultCommandsUnitTest {
         TestCommandContext ctx = new TestCommandContext();
         cmd.execute(ctx);
         Assert.assertTrue(ctx.getCommandWriter().getOutput().contains("test.timestamp"), "Test output did not contain 'test.timestamp'");
-        Assert.assertTrue(ctx.getCommandWriter().getOutput().contains(stamp), "Test output did not contain "+stamp);
+        Assert.assertTrue(ctx.getCommandWriter().getOutput().contains(stamp), "Test output did not contain " + stamp);
     }
+
+
+    @Test
+    public void testShellCommandWithNoArgs() throws Exception {
+        DefaultPlugin.ShellCommand cmd = new DefaultPlugin.ShellCommand();
+        TestCommandContext ctx = new TestCommandContext();
+        ctx.setCommandArguments(new String[0]);
+        cmd.execute(ctx);
+        Assert.assertTrue(ctx.getCommandWriter().getOutput().contains("The sh command expects a command which you would like to execute"), "Incorrect output");
+        Assert.assertFalse(ctx.getCommandWriter().getOutput().contains("java.lang.ArrayIndexOutOfBoundsException"), "Incorrect output");
+    }
+
 
 }
