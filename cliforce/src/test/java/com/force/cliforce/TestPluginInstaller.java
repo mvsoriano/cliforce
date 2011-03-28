380e40d22df7944afd576cc9e06b39309e8ce046
sclasen <sclasen@salesforce.com>
2011-03-28 15:30:47 -0700

    doc

--- cliforce/src/test/java/com/force/cliforce/TestPluginInstaller.java
+++ cliforce/src/test/java/com/force/cliforce/TestPluginInstaller.java
@@ -4,6 +4,13 @@ package com.force.cliforce;
 import javax.inject.Inject;
 import java.io.IOException;
 
+/**
+ * Allows tests to manually install plugins to a pluginManager
+ * rather than loading them through the DependencyResolver/ServiceLoader
+ * mechanism of the PluginCommand.
+ * <p/>
+ * The pluginManager used is the same one injected into the cliforce instance if you are using the TestModule guice mod.
+ */
 public class TestPluginInstaller {
 
     @Inject
