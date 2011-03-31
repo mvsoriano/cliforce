1e9975c90d8d437d6f4edd46f4f7352f6efcbeab
John Simone <jsimone@salesforce.com>
2011-03-31 14:13:01 -0700

    W-924907 Unit Test

--- plugins/template/src/test/java/com/force/cliforce/plugin/template/command/TemplatePluginUnitTest.java
+++ plugins/template/src/test/java/com/force/cliforce/plugin/template/command/TemplatePluginUnitTest.java
@@ -4,6 +4,10 @@ package com.force.cliforce.plugin.template.command;
 import com.force.cliforce.TestCommandContext;
 import com.force.cliforce.TestModule;
 import com.google.inject.Guice;
+import com.sforce.async.RestConnection;
+import com.sforce.soap.metadata.MetadataConnection;
+import com.sforce.soap.partner.PartnerConnection;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -26,4 +30,40 @@ public class TemplatePluginUnitTest {
         Assert.assertTrue(proj.exists());
         Assert.assertTrue(new File(proj, "pom.xml").exists());
     }
+    
+
+    @Test
+    public void templateConnectionExceptions() throws Exception {
+        NewProjectCommand cmd = Guice.createInjector(new TestModule()).getInstance(NewProjectCommand.class);
+        File workingDir = new File(new File(System.getProperty("user.home")), "template-test");
+        if (!workingDir.exists()) {
+            workingDir.mkdir();
+        }
+        String project = "template" + Long.toString(System.currentTimeMillis());
+        TestCommandContext ctx = new TestCommandContextConnectionExceptions().withCommandArguments("--dir", workingDir.getAbsolutePath(), project);
+        //As long as this doesn't throw an exception and the command still works the test passes
+        cmd.execute(ctx);
+        File proj = new File(workingDir, project);
+        Assert.assertTrue(proj.exists());
+        Assert.assertTrue(new File(proj, "pom.xml").exists());
+    }
+    
+    private class TestCommandContextConnectionExceptions extends TestCommandContext {
+
+		@Override
+		public MetadataConnection getMetadataConnection() {
+			throw new RuntimeException();
+		}
+
+		@Override
+		public PartnerConnection getPartnerConnection() {
+			throw new RuntimeException();
+		}
+
+		@Override
+		public RestConnection getRestConnection() {
+			throw new RuntimeException();
+		}
+    	
+    }    
 }
