4c18e486ddd61378cb23ef7516419ede57bbaa0c
Jeff Lai <jeffrey.lai@salesforce.com>
2011-04-01 10:48:17 -0700

    collapse BaseCliforceTest and BaseCliforceCommandTest to make test code easier to maintain

--- cliforce/src/test/java/com/force/cliforce/BaseCliforceCommandTest.java
+++ cliforce/src/test/java/com/force/cliforce/BaseCliforceCommandTest.java
@@ -1,8 +1,37 @@
 package com.force.cliforce;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
-public abstract class BaseCliforceCommandTest extends BaseCliforceTest {
+import javax.servlet.ServletException;
+
+import org.testng.annotations.BeforeClass;
+
+import com.google.inject.Guice;
+import com.google.inject.Module;
+import com.sforce.ws.ConnectionException;
+
+public abstract class BaseCliforceCommandTest {
+    
+    private CLIForce cliForce;
+    private ByteArrayOutputStream baos;
+    
+    @BeforeClass
+    public void classSetup() throws InterruptedException, IOException, ConnectionException, ServletException {
+        Module guiceModule = setupTestModule();
+        cliForce = Guice.createInjector(guiceModule).getInstance(CLIForce.class);
+        baos = new ByteArrayOutputStream();
+        InputStream in = new ByteArrayInputStream(new byte[] {});
+        cliForce.init(in, new PrintWriter(baos, true));
+        setupCLIForce(cliForce);
+    }  
+    
+    public TestModule setupTestModule() {
+        return new TestModule();
+    }
 
     public abstract String getPluginArtifact();
     
@@ -12,7 +41,21 @@ public abstract class BaseCliforceCommandTest extends BaseCliforceTest {
         return true;
     }
     
-    @Override
+    protected String executeCommand(String... cmd) throws IOException, ConnectionException, ServletException, InterruptedException {
+        baos.reset();
+        cliForce.executeWithArgs(cmd);
+        String outputStr = new String(baos.toByteArray());
+        return outputStr;
+    }
+    
+    public String runCommand(String cmd) throws IOException, ConnectionException, ServletException, InterruptedException {
+        return executeCommand(cmd.split(" "));
+    }
+    
+    public CLIForce getCLIForce() {
+        return cliForce;
+    }
+    
     public void setupCLIForce(CLIForce c) throws IOException {
       c.installPlugin(getPluginArtifact(), "LATEST", getPlugin(), isInternal());
     }
