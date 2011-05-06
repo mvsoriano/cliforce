02414cb8cdc6dac8ed25e37f71b40d29b1d1b317
sclasen <sclasen@salesforce.com>
2011-05-06 12:38:15 -0700

    W-950726 remove db:clean

--- plugins/db/src/main/java/com/force/cliforce/plugin/db/DBPlugin.java
+++ plugins/db/src/main/java/com/force/cliforce/plugin/db/DBPlugin.java
@@ -1,17 +1,16 @@
 package com.force.cliforce.plugin.db;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.force.cliforce.Command;
 import com.force.cliforce.Plugin;
-import com.force.cliforce.plugin.db.command.DBClean;
 import com.force.cliforce.plugin.db.command.Describe;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class DBPlugin implements Plugin {
 
     @Override
     public List<Class<? extends Command>> getCommands() {
-        return Arrays.asList(DBClean.class, Describe.class);
+        return Arrays.<Class<? extends Command>>asList(Describe.class);
     }
 }
