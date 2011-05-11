9c29dda77f69f6e614f01cb6a23d173319d9f806
Naaman Newbold <nnewbold@salesforce.com>
2011-05-11 12:21:20 -0700

    W-949215 cleaned up tests to use dataprovider

--- plugins/jpa/src/test/java/com/force/cliforce/plugin/jpa/JPACommandTest.java
+++ plugins/jpa/src/test/java/com/force/cliforce/plugin/jpa/JPACommandTest.java
@@ -30,10 +30,14 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.force.cliforce.Command;
 import com.force.cliforce.TestCommandContext;
 import com.force.cliforce.TestCommandReader;
 import com.force.cliforce.plugin.jpa.command.*;
 import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 
@@ -50,97 +54,54 @@ public class JPACommandTest extends JPAPluginBaseTest {
 
     @DataProvider
     public Object[][] jpaCommandWithArgs() {
+        String[] testFixtureProjectArgs = new String[]{ "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t" };
+
         return new Object[][] {
-                // command, ordered input, query, result,
-                { JPAPopulate.class, Lists.newArrayList("1", "3") }
+              // format: command, ordered input, query, result, command args, expected result
+              /*  POSITIVE TESTS  */
+                {
+                        JPAPopulate.class, null, null, null, new String[]{ "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t", "-u", "testDNJpaPersistence" },
+                        "Running with selected PersistenceUnit: testDNJpaPersistence"
+                }
+              , { JPAPopulate.class, getReader("1", "3"), null, null, testFixtureProjectArgs, "[1-5] q to quit? 3\nRunning with selected PersistenceUnit: testDNJpaPersistence" }
+              , { JPAPopulate.class, getReader("1", "q"), null, null, testFixtureProjectArgs, "[1-5] q to quit? q" }
+              , { JPAPopulate.class, getReader("1", "", "q"), null, null, testFixtureProjectArgs, "[1-5] q to quit? \n[1-5] q to quit? q\n" }
+              , { JPAPopulate.class, getReader("1", "0", "q"), null, null, testFixtureProjectArgs, "[1-5] q to quit? 0\n[1-5] q to quit? q\n" }
+              , { JPAClean.class, getReader("1", "3"), null, null, testFixtureProjectArgs, "[1-5] q to quit? 3\nRunning with selected PersistenceUnit: testDNJpaPersistence" }
+              , { JPAQuery.class, getReader("1", "3", "select o from Account o", "q"), "select o from Account o", Lists.newArrayList(), testFixtureProjectArgs, "jpql (q to quit) > select o from Account o\nNo data found\njpql (q to quit) > q"}
+              /*  NEGATIVE TESTS  */
+
+                // TODO: force a persistenceexception when the whole thing is mocked
+//              , { JPAQuery.class, getReader("1", "3", "select o from Account o", "q"), "select o from Account o", Lists.newArrayList(), testFixtureProjectArgs, "javax.persistence.PersistenceException: Class Account for query has not been resolved. Check the query and any imports specification"}
         };
     }
 
-    /**
-     * This test will check basic PersistenceUnit selection that is common to all JPA commands
-     * @throws Exception
-     */
-    @Test
-    public void testJPAPluginCommandParams() throws Exception {
-        // Check all base params
-        TestCommandContext ctx = createCtxWithJPA(JPAPopulate.class, new TestCommandReader(Lists.newArrayList("1", "3")), null, null,
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        validateConnectToOrg(ctx);
-        validatePUSelection(ctx);
-        
-        // try the 'q' quit command
-        ctx = createCtxWithJPA(JPAPopulate.class, new TestCommandReader(Lists.newArrayList("1", "q")), null, null,
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        validateConnectToOrg(ctx);
-        Assert.assertTrue(ctx.getCommandWriter().getOutput().endsWith(
-                "Select PersistenceUnit:\n" +
-                "1. SchemaLoadInvocationFTest\n" +
-                "2. extPersCtxPU\n" +
-                "3. testDNJpaPersistence\n" +
-                "4. testDNJpaPersistence2\n" +
-                "5. testDNJpaPersistence3\n" +
-                "[1-5] q to quit? q\n"), ctx.getCommandWriter().getOutput());
-
-        // Pass in persistenceunit and expect no selection
-        ctx = createCtxWithJPA(JPAPopulate.class, null, null, null,
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t", "-u", "testDNJpaPersistence");
-        validateConnectToOrg(ctx);
-        Assert.assertTrue(ctx.getCommandWriter().getOutput().endsWith(
-                "Running with selected PersistenceUnit: testDNJpaPersistence\n"), ctx.getCommandWriter().getOutput());
-    }
-    
-    @Test
-    public void testJPAPluginNegativeParams() throws Exception {
-        // Check a missing base param
-        TestCommandContext ctx = createCtxWithJPA(JPAPopulate.class, new TestCommandReader(Lists.newArrayList("1", "3")), null, null,
-                "-g", "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        Assert.assertTrue(ctx.getCommandWriter().getOutput().startsWith(
-        "Exception while executing command: populate"), ctx.getCommandWriter().getOutput());
-        
-        // try no selection followed by invalid selection
-        ctx = createCtxWithJPA(JPAPopulate.class, new TestCommandReader(Lists.newArrayList("1", "", "0", "q")), null, null,
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        validateConnectToOrg(ctx);
-        Assert.assertTrue(ctx.getCommandWriter().getOutput().endsWith(
-                "Select PersistenceUnit:\n" +
-                "1. SchemaLoadInvocationFTest\n" +
-                "2. extPersCtxPU\n" +
-                "3. testDNJpaPersistence\n" +
-                "4. testDNJpaPersistence2\n" +
-                "5. testDNJpaPersistence3\n" +
-                "[1-5] q to quit? \n" +
-                "[1-5] q to quit? 0\n" +
-                "[1-5] q to quit? q\n"), ctx.getCommandWriter().getOutput());
-    }
-    
-    @Test
-    public void testJPAPluginPopulate() throws Exception {
-        TestCommandContext ctx = createCtxWithJPA(JPAPopulate.class, new TestCommandReader(Lists.newArrayList("1", "3")), null, null,
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        validateConnectToOrg(ctx);
-        validatePUSelection(ctx);
-    }
-    
-    @Test
-    public void testJPAPluginClean() throws Exception {
-        TestCommandContext ctx = createCtxWithJPA(JPAClean.class, new TestCommandReader(Lists.newArrayList("1", "3")), null, null,
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        validateConnectToOrg(ctx);
-        validatePUSelection(ctx);
-    }
-    
-    @Test
-    public void testJPAPluginQuery() throws Exception {
-        // Run some JPQL
-        TestCommandContext ctx = createCtxWithJPA(JPAQuery.class, new TestCommandReader(Lists.newArrayList("1", "3", "select o from Account o", "q")),
-                "select o from Account o",
-                Lists.newArrayList(),
-                "-g", TEST_GROUP, "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
-        validateConnectToOrg(ctx);
-        validatePUSelection(ctx);
+    @Test(dataProvider = "jpaCommandWithArgs")
+    public void testJPACommand(Class<? extends Command> command, TestCommandReader reader, String query, List<?> queryResult, String[] testFixtureProjectArgs, String expectedSubstring) throws Exception {
+        TestCommandContext testContext = createCtxWithJPA(command, reader, query, queryResult, testFixtureProjectArgs);
+        verifyConnection(testContext);
+        assertStringContains(testContext.out(), expectedSubstring);
     }
 
-    private void validateConnectToOrg(TestCommandContext ctx) {
+    @Test
+    public void testInvalidMavenArgs() throws Exception {
+        TestCommandContext testContext = createCtxWithJPA(JPAPopulate.class, getReader("1", "3"), null, null, "-g", "-a", TEST_ARTIFACT, "-v", TEST_VERSION, "-t");
+        assertStringContains(testContext.out(), "Exception while executing command: populate");
+    }
+
+    private void assertStringContains(String actual, String expectedSubstring) {
+        Assert.assertTrue(
+                actual.contains(expectedSubstring)
+                , "Expected substring: " + expectedSubstring + "\nActual substring: " + actual
+        );
+    }
+
+    private TestCommandReader getReader(String... inputs) {
+        ArrayList inputList = Lists.newArrayList(inputs);
+        return new TestCommandReader(inputList);
+    }
+
+    private void verifyConnection(TestCommandContext ctx) {
         Assert.assertTrue(ctx.getCommandWriter().getOutput().contains("Connected to org mock_organization_id"), ctx.getCommandWriter().getOutput());
     }
 
